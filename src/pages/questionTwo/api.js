673a9701861e538913345f702d4b4ebad7016420
Bahtiyar <bahtiyar@outlook.com>
2022-01-27 01:44:22 +0300

    resolve question two api call problem

--- src/pages/questionTwo/api.js
+++ src/pages/questionTwo/api.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-async function getCardDetails (){
-	let newPromise = new Promise((resolve, reject)=>{
-		axios.get('http://localhost:3001/example').then((result)=>{
-			const {title, body, imgSrc} = result.data;
-			resolve(title, body, imgSrc)
-		})
-	})
-	return newPromise
+async function getCardDetails() {
+    return new Promise((resolve, reject) => {
+        axios.get("http://localhost:3001/example").then((result) => {
+            const { title, body, imgSrc } = result.data;
+            resolve({ title, body, imgSrc });
+        });
+    });
 }
 
-export { getCardDetails }
+export { getCardDetails };
